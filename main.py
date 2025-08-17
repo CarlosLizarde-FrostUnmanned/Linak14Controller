13610a017b8f0a9e8590fb2d81ab9fd509cb7bd3
CarlosLizarde-FrostUnmanned <carlos.lizarde@frostunmanned.com>
2025-08-17 14:51:14 +0200

    testing without the new logic

--- main.py
+++ main.py
@@ -91,31 +91,32 @@ class LinakController:
                 return False
 
             message = can.Message(arbitration_id=can_id, data=data, is_extended_id=True)
+            self.bus.send(message)
 
-            if command == LinakCommand.ALL_IN or command == LinakCommand.ALL_OUT:
+            # if command == LinakCommand.ALL_IN or command == LinakCommand.ALL_OUT:
 
-                reached = False
-                counterLimit = 30000
-                counter = 0
+            #     reached = False
+            #     counterLimit = 30000
+            #     counter = 0
 
-                while (reached != True):
+            #     while (reached != True):
 
-                    self.bus.send(message)
+            #         self.bus.send(message)
 
-                    counter = counter +1
-                    with can.Bus() as bus:
-                        for msg in bus:
-                            print(msg.data)
+            #         counter = counter +1
+            #         with can.Bus() as bus:
+            #             for msg in bus:
+            #                 print(msg.data)
 
-                            source_id_str = hex(msg.arbitration_id)[7] + hex(msg.arbitration_id)[8]
+            #                 source_id_str = hex(msg.arbitration_id)[7] + hex(msg.arbitration_id)[8]
 
-                            if source_id_str == target_id_str:
-                                print(msg.data)
-                                #reached = True
-                            if msg.data or counter > counterLimit:
-                                break
-            else:
-                self.bus.send(message)
+            #                 if source_id_str == target_id_str:
+            #                     print(msg.data)
+            #                     #reached = True
+            #                 if msg.data or counter > counterLimit:
+            #                     break
+            # else:
+            #     self.bus.send(message)
 
             print(f"Sent command {command.name} to actuator {can_id}: {data}")
             return True
